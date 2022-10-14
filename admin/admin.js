2bfde9dc153c8f44c6d0e8fc8d4cd83980a38aa6
Ahmar khan <ahmarbilal223344@gmail.com>
2022-10-14 23:51:16 +0500

    addition done

--- admin/admin.js
+++ admin/admin.js
@@ -6,7 +6,26 @@ const btn = document.getElementById("button");
 const form = document.getElementById("form");
 const menu = document.getElementById("menu");
 
-const menuItems = [];
+const menuItems = [
+  {
+    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/111-1627418162-chicken-tikka.jpeg",
+    title: "Chiken tandori tikka",
+    detail: "Spicy Pakistani Tikka chunks & Onions",
+    price: 499,
+  },
+  {
+    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/132-1627418066-Fajita-Sicilian.jpeg",
+    title: "Chiken BBQ",
+    detail: "Spicy Pakistani Tikka chunks & Onions",
+    price: 399,
+  },
+  {
+    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/132-1627418066-Fajita-Sicilian.jpeg",
+    title: "Chiken BBQ",
+    detail: "Spicy Pakistani Tikka chunks & Onions",
+    price: 299,
+  },
+];
 
 const loadData = () => {
   menu.innerHTML = "";
--- index.js
+++ index.js
@@ -1,25 +1,5 @@
+import menuItems from "./module-menu.js";
 const menu = document.getElementById("menu");
-
-const menuItems = [
-  {
-    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/111-1627418162-chicken-tikka.jpeg",
-    title: "Chiken tandori tikka",
-    detail: "Spicy Pakistani Tikka chunks & Onions",
-    price: 499,
-  },
-  {
-    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/132-1627418066-Fajita-Sicilian.jpeg",
-    title: "Chiken BBQ",
-    detail: "Spicy Pakistani Tikka chunks & Onions",
-    price: 399,
-  },
-  {
-    img: "https://ph-web-bucket.s3.us-east-2.amazonaws.com/data/img/products/images/132-1627418066-Fajita-Sicilian.jpeg",
-    title: "Chiken BBQ",
-    detail: "Spicy Pakistani Tikka chunks & Onions",
-    price: 299,
-  },
-];
 
 menuItems.map((item) => {
   menu.innerHTML += `
